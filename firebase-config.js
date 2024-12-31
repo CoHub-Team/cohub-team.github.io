db8926faccba475c176431c477b3477c445a2c6d
abdklncc <102408934+abdklncc@users.noreply.github.com>
2024-12-31 10:40:52 +0300

    Deleted the keys

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA6qWJZ-p5gUpJI_h8TfU5WYg1ACoYcT6g",
-    authDomain: "frigoo-4b31c.firebaseapp.com",
-    projectId: "frigoo-4b31c",
-    storageBucket: "frigoo-4b31c.firebasestorage.app",
-    messagingSenderId: "429881150999",
-    appId: "1:429881150999:web:edc24070c394b4f7811652",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 };
 
 // Firebase'i başlat
